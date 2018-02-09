794cfd2a134e832574fc18648dc2fecf80977b69
Jason Lane <quizathaderat@yahoo.com>
2018-02-09 11:54:04 -0800

    Begin installing apollo

--- src/app.tsx
+++ src/app.tsx
@@ -9,10 +9,19 @@ import pageShell from "./components/page_shell"
 import { AppState } from "./redux/core"
 import * as actions from "./redux/actions"
 import { getAccessToken } from "./util/access_token_utils"
+import { ApolloClient } from "apollo-client"
+import { HttpLink } from "apollo-link-http"
+import { InMemoryCache } from "apollo-cache-inmemory"
+import { ApolloProvider } from "react-apollo";
 
 // pages
 import LoginPage from "./pages/login/login_page"
 import Dashboard from "./pages/dashboard/dashboard"
+
+const client = new ApolloClient({
+    link: new HttpLink({ uri: "http://localhost:8090" }),
+    cache: new InMemoryCache()
+})
 
 interface ReduxState {
 }
@@ -47,12 +56,14 @@ class App extends React.Component<ApplicationProps, ApplicationState> {
 
     render(): JSX.Element {
         return (
-            <ConnectedRouter history={history}>
-                <div>
-                    <PublicRoute path="/" component={pageShell(LoginPage)}/>
-                    <PrivateRoute path="/dashboard" component={pageShell(Dashboard)}/>
-                </div>
-            </ConnectedRouter>
+            <ApolloProvider client={client}>
+                <ConnectedRouter history={history}>
+                    <div>
+                        <PublicRoute path="/" component={pageShell(LoginPage)}/>
+                        <PrivateRoute path="/dashboard" component={pageShell(Dashboard)}/>
+                    </div>
+                </ConnectedRouter>
+            </ApolloProvider>
         )
     }
 }
